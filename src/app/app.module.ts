0c6c10b70ac108a886fb0a31150e739fd8c34722
Yashraj Dawkhar <dawkharyashraj@gmail.com>
2023-05-11 02:33:29 +0530

    login update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,9 +15,13 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    ToastModule,
+    InputTextModule,
+    ButtonModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  error: string = ''
+
+  login: any = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  })
+
+  LoginData(data: any) {
+
+    Object.keys(this.login.controls).forEach((key) => {
+      this.login.controls[key].markAsDirty();
+    });
+    if (this.login.valid) {
+      // this.backend.IntelliAssistLogin(data.email, data.password).subscribe({
+      // 	next: (data: any) => {
+      // 		if (data.successOrErrorCode != 0) {
+      // 			this.error = data.message
+      // 		} else {
+      // 			let message = this.backend.UserRedirect(data)
+      // 			this.messageService.add({ severity: message.key, summary: 'Success', detail: message.value });
+      // 		}
+      // 	},
+      // 	error: () => {
+      // 		this.messageService.add({ severity: 'warn', summary: 'Failure', detail: "Authentication Api Failed" });
+      // 	}
+      // })
+    }
+  }
+
+  removeError(email: any) {
+    this.error = ''
+  }
 
 }
